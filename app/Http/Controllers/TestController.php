154c830196d13348d4f6b4dfb3a40451bdb8f8a1
Jasvir123 <80539562+Jasvir123@users.noreply.github.com>
2024-02-23 16:44:31 +0530

    Revert "add about us page (#4)" (#5)
    This reverts commit 8299bf6968f101681668705b09c7d82d9f5db76a.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,4 @@ class TestController extends Controller
     public function products(){
         return view('products');
     }
-    public function aboutUs(){
-        return vi ew('about-us');
-    }
 }
